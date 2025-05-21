337d6716d6d6e3d1d83ced25d8e393acd1488cd7
0mniManTheCpu <mymhlu001@myuct.ac.za>
2025-05-21 03:13:29 +0200

    Added comments and changed namings of some variables

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,25 +3,40 @@
 // 21 May 2025
 
 public class BinarySearch {
-    public static int binarySearch(int[] array, int target, int left, int right) {
-        int middle = (left + right) / 2;
-        if (left > right) {
+    public static int binarySearch(int[] array, int target, int left_pointer, int right_pointer) {
+        // targeting the middle value by finding it's index
+        int middle = (left_pointer + right_pointer) / 2;
+
+        // if out of bound then return index of where the value would be if it were to
+        // be added
+        if (left_pointer > right_pointer) {
             return -1;
         }
+
+        // check if the middle value is equal to the target
         if (array[middle] == target) {
             return middle;
         }
 
+        // checking if the middle value is greater than the target
         if (array[middle] > target) {
-            return binarySearch(array, target, left, middle - 1);
+            // recursive call moving the right index into middle - 1
+            return binarySearch(array, target, left_pointer, middle - 1);
         }
-        return binarySearch(array, target, middle + 1, right);
+
+        // recursive call moving the left pointer if middle value is lesser than target
+        return binarySearch(array, target, middle + 1, right_pointer);
     }
 
     public static void main(String[] args) {
+        // sorted array to search for the target
         int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        // pointers
         int LEFT = 0;
         int RIGHT = numbers.length - 1;
+
+        // test cases
         System.out.println(binarySearch(numbers, 10, LEFT, RIGHT)); // output = -1
         System.out.println(binarySearch(numbers, 1, LEFT, RIGHT)); // output = 0
         System.out.println(binarySearch(numbers, 5, LEFT, RIGHT)); // output = 4
